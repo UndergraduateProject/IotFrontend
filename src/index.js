315a8777cf2bc0b67c698a2c8c44c01f2dddbc85
root <root@ip7198.cm.nsysu.edu.tw>
2021-04-14 14:36:58 +0800

    add .env file for deployment

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM, { render } from 'react-dom';
 import './index.css';
 import axios from 'axios'; //這裡需要安裝package  "npm install axios" 
 import { Line } from 'react-chartjs-2';
-const url = "http://140.117.71.98:8000/enviroment/humidity/";
+const url = "http://140.117.71.98:8000/api/humidity/";
 
 let linedata = {
   labels: [], //每一個點的時間點
@@ -17,7 +17,7 @@ let linedata = {
 
 
 function App() {
-  const [data, setData] = useState ([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     getdata();
@@ -27,7 +27,7 @@ function App() {
     const response = await axios.get(url);
     let humidity = [];
     console.log(response.data);
-    response.data['results'].forEach(ele => humidity.push(ele) );
+    response.data['results'].forEach(ele => humidity.push(ele));
     humidity.forEach(
       ele => {
         linedata.datasets[0].data.push(ele.value)
@@ -38,18 +38,18 @@ function App() {
   };
 
 
-  
+
   return (
     <div>
-      <ul>{data.map((x,i) => <li key={i}>id : {x.id}   humidity : {x.value}     created : {x.created}</li>)}</ul>
-      <Line data={linedata}/>
+      <ul>{data.map((x, i) => <li key={i}>id : {x.id}   humidity : {x.value}     created : {x.created}</li>)}</ul>
+      <Line data={linedata} />
     </div>
   )
 }
-  
+
 // ========================================
-  
+
 ReactDOM.render(
-	<App />,
-	document.getElementById('root')
+  <App />,
+  document.getElementById('root')
 );
