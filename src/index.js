2ab1b060a733b0d9e37b12c18e7e8d180eb02f4c
AngelHuang1998 <74661707+AngelHuang1998@users.noreply.github.com>
2021-04-20 02:07:32 +0800

    angel1
    log in/sign in

--- src/index.js
+++ src/index.js
@@ -1,56 +1,12 @@
-import React, { useState, useEffect } from "react";
-import ReactDOM, { render } from 'react-dom';
-import './index.css';
-import axios from 'axios'; //這裡需要安裝package  "npm install axios" 
-import { Line } from 'react-chartjs-2';
-const url = "http://140.117.71.98:8000/enviroment/humidity/";
-
-let linedata = {
-  labels: [], //每一個點的時間點
-  datasets: [
-    {
-      label: 'Humidity',
-      data: [] //濕度資料儲存位置
-    }
-  ]
-};
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
 
-function App() {
-  const [data, setData] = useState ([]);
-
-  useEffect(() => {
-    getdata();
-  }, []);
-
-  const getdata = async () => {
-    const response = await axios.get(url);
-    let humidity = [];
-    console.log(response.data);
-    response.data['results'].forEach(ele => humidity.push(ele) );
-    humidity.forEach(
-      ele => {
-        linedata.datasets[0].data.push(ele.value)
-        linedata.labels.push(ele.created)
-      }
-    )
-    // hihihi
-    setData(humidity);
-  };
-
-
-  
-  return (
-    <div>
-      <ul>{data.map((x,i) => <li key={i}>id : {x.id}   humidity : {x.value}     created : {x.created}</li>)}</ul>
-      <Line data={linedata}/>
-    </div>
-  )
-}
-  
-// ========================================
-  
 ReactDOM.render(
-	<App />,
-	document.getElementById('root')
+  <React.StrictMode>
+    <App />	
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
