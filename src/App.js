a46f6d75a60dc58566c960077104945de0e5e283
william50307 <william5030788@gmail.com>
2021-04-21 01:20:55 +0800

    add api.js, post sign up data to database using api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import { useSpring, animated } from "react-spring";
-
+import api from './utils/api'; // import api function which contains "GET","POST","DELETE","PATCH"
 
 function App() {
   const [registrationFormStatus, setRegistartionFormStatus] = useState(false);
@@ -23,10 +23,11 @@ function App() {
       : "solid 0px transparent", //Animate bottom border of register button
   });
 
-  function registerClicked() {
+  // 像這種在裡面的function盡量寫成arrow function
+  const registerClicked = () => {
     setRegistartionFormStatus(true);
   }
-  function loginClicked() {
+  const loginClicked = () => {
     setRegistartionFormStatus(false);
   }
 
@@ -54,12 +55,13 @@ function App() {
         <animated.form action="" id="loginform" style={loginProps}>
           <LoginForm />
         </animated.form>
-        <animated.form action="" id="registerform" style={registerProps}>
-          <RegisterForm />
-        </animated.form>
+        {/* 把amimated.form這個component加入RegisterForm裡面 透過porps傳入style 上面的loginform也按照一樣的做法 */ }
+        {/* <animated.form action="" id="registerform" style={registerProps}> */}   
+          <RegisterForm style={registerProps} />
+        {/* </animated.form> */}
       </div>
       <animated.div className="forgot-panel" style={loginProps}>
-        <a herf="#">Forgot password</a>
+        <a herf="#">Forgot password</a>    
       </animated.div>
     </div>
   );
@@ -77,16 +79,51 @@ function LoginForm() {
   );
 }
 
-function RegisterForm() {
-  return (
-    <React.Fragment>
-      <input id="fullname" type="text" placeholder="Create your username" />
-      <input id="email" type="text" placeholder="Create your email" />
-      <input id="password" type="text" placeholder="Create your password" />
-      <input id="confirmpassword" type="text" placeholder="Enter  your password again" />
+function RegisterForm(props) { //設定props參數，取得從外面傳進來的style  
+  const [formdata, setFormdata] = useState({  // a state contains form data
+    username : '',
+    email : '',
+    password : '',
+    confirmpassword : ''
+  });
+ 
+  const handleChange = e => {
+    const {id , value} = e.target;    // 特殊用法，類似python tuple unpack
+    setFormdata( prevState => ({      //特殊用法，keyword : object spread
+      ...prevState,
+      [id] : value,       
+    }));
+  }
+  
+  const postclient = (path) => {
+    const data = { 
+      username : formdata.username,
+      email : formdata.email,
+      password : formdata.password,
+      verify : 99999,  //for testing
+    }
+    api.post(path, data) //post data to server/database
+    .then(res => {
+      console.log(res);  //print response data
+    })
+  }
 
-      <input type="submit" value="submit" class="submit" />
-    </React.Fragment>
+  const handleSubmit = e => {
+    e.preventDefault();
+    postclient('client/'); //api url
+  }
+
+  return (
+    <animated.form action="" id="registerform" style={props.style}> 
+    {/* <React.Fragment>  Now we can delete this fragment*/} 
+      <input id="username" type="text" placeholder="Create your username" value={formdata.username} onChange={handleChange} />
+      <input id="email" type="text" placeholder="Create your email" value={formdata.email} onChange={handleChange}/>
+      <input id="password" type="text" placeholder="Create your password" value={formdata.password} onChange={handleChange} />
+      <input id="confirmpassword" type="text" placeholder="Enter  your password again" value={formdata.confirmpassword} onChange={handleChange} />
+
+      <input type="submit" value="submit" class="submit" onClick={handleSubmit}/>
+    {/* </React.Fragment> */}
+    </animated.form>
   );
 }
 
