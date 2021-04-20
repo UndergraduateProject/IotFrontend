32bcfaf038218e4e29ab022a457f2114c3945ef6
Nyc <s2012439@yes.my>
2021-04-20 14:35:01 +0800

    chart js and css

---
+++ src/chart.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM, { render } from 'react-dom';
+import './chart.css';
+import axios from 'axios'; //這裡需要安裝package  "npm install axios" 
+import { Line } from 'react-chartjs-2';
+const url = "http://140.117.71.98:8000/api/humidity/";
+
+let linedata = {
+  labels: [], //每一個點的時間點
+  datasets: [
+    {
+      label: 'Humidity',
+      data: [] //濕度資料儲存位置
+    }
+  ]
+};
+
+
+function Chart() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getdata();
+  }, []);
+
+  const getdata = async () => {
+    const response = await axios.get(url);
+    let humidity = [];
+    console.log(response.data);
+    response.data['results'].forEach(ele => humidity.push(ele));
+    humidity.forEach(
+      ele => {
+        linedata.datasets[0].data.push(ele.value)
+        linedata.labels.push(ele.created)
+      }
+    )
+    setData(humidity);
+  };
+
+
+
+  return (
+    <div>
+      <ul>{data.map((x, i) => <li key={i}>id : {x.id}   humidity : {x.value}     created : {x.created}</li>)}</ul>
+      <Line data={linedata} />
+    </div>
+  )
+}
+
+export default Chart
