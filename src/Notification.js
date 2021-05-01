208ddd9c1c52a13aaddb7fb8a7633c924ca03387
Nyc <s2012439@yes.my>
2021-05-01 18:59:26 +0800

    Added Notification, npm install to update packages

---
+++ src/Notification.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
+import ReactNotification from 'react-notifications-component'
+import {store} from 'react-notifications-component';
+import 'animate.css'
+import 'react-notifications-component/dist/theme.css'
+
+class Notification extends Component {
+  constructor() {
+    super();
+    this.state = {
+      response: false,
+      endpoint: "http://140.117.71.98:4001"
+    };
+  }
+
+
+componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on("message", data =>{
+      this.setState({ response: data })
+      store.addNotification({
+        title: "Warning!",
+        message: "Temperature too high :" + data,
+        type: "danger",
+        insert: "top",
+        container: "top-center",
+        animationIn: ["animate__animated", "animate__fadeIn"],
+        animationOut: ["animate__animated", "animate__fadeOut"],
+        dismiss: {
+          duration: 5000,
+          onScreen: true
+        }
+      });
+    }
+    
+    );
+    
+  }
+
+render() {
+    const { response } = this.state;
+    return (
+      <div style={{ textAlign: "center" }}>
+      <ReactNotification/>
+      </div>
+    );
+  }
+}
+export default Notification;
--- src/chart.js
+++ src/chart.js
@@ -3,22 +3,34 @@ import ReactDOM, { render } from 'react-dom';
 import './chart.css';
 import axios from 'axios'; //這裡需要安裝package  "npm install axios" 
 import { Line } from 'react-chartjs-2';
-const url = "http://140.117.71.98:8000/api/humidity/";
+const url = "http://140.117.71.98:8000/api/humidtemp/";
 
-let linedata = {
-  labels: [], //每一個點的時間點
+const linedata = {
+  labels: [],
   datasets: [
     {
       label: 'Humidity',
-      data: [] //濕度資料儲存位置
-    }
-  ]
+      data: [],
+      fill: false,
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgba(255, 99, 132, 0.2)',
+      yAxisID: 'y-axis-1',
+    },
+    {
+      label: 'Temperature',
+      data: [],
+      fill: false,
+      backgroundColor: 'rgb(54, 162, 235)',
+      borderColor: 'rgba(54, 162, 235, 0.2)',
+      yAxisID: 'y-axis-2',
+    },
+  ],
 };
 
 
 function Chart() {
   const [data, setData] = useState([]);
-
+  const [view,setView]  = useState(true);
   useEffect(() => {
     getdata();
   }, []);
@@ -27,22 +39,31 @@ function Chart() {
     const response = await axios.get(url);
     let humidity = [];
     console.log(response.data);
+   
     response.data['results'].forEach(ele => humidity.push(ele));
     humidity.forEach(
       ele => {
-        linedata.datasets[0].data.push(ele.value)
+        linedata.datasets[0].data.push(ele.humidity)
+        linedata.datasets[1].data.push(ele.temperature)
         linedata.labels.push(ele.created)
       }
     )
     setData(humidity);
   };
-
-
-
+  
+  if(view){
+    var msg = "show chart";
+  }
+  else{
+    var msg = "show raw data";
+  }
   return (
     <div>
-      <ul>{data.map((x, i) => <li key={i}>id : {x.id}   humidity : {x.value}     created : {x.created}</li>)}</ul>
-      <Line data={linedata} />
+      {
+        view ? <ul>{data.map((x, i) => <li key={i}>id : {x.id}   humidity : {x.humidity}  temperature : {x.temperature}   created : {x.created}</li>)}</ul> 
+        : <Line  data={linedata} />
+      }
+      <button onClick={()=> setView(!view)}>{msg}</button>
     </div>
   )
 }
