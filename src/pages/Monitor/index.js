f2a85ed7f127a351d8a66352b4ee90d1b509d1ec
Nameyc99 <s2012439@yes.my>
2021-06-21 16:15:30 +0800

    server debug push(don't change on server too much)

--- src/pages/Monitor/index.js
+++ src/pages/Monitor/index.js
@@ -11,45 +11,45 @@ import monitor_arrow_right from './monitor_arrow_right.png';
 
 
 
-function monitor() {
+function Monitor() {
   return (
     <div className="body_monitor">
 
       <div className="monitor_top">Green House</div>
       <div className="monitor_top1">Monitoring Status</div>
-      
-      <img className="monitor_wea_pic" src={monitor_weather}/>
+
+      <img className="monitor_wea_pic" src={monitor_weather} />
       <div className="monitor_top3">晴時多雲</div>
-      
+
 
       <div className="wrap_light">
-        <img className="monitor_light_pic" src={monitor_light1}/>
-          <div className="monitor_top2">40%</div>
+        <img className="monitor_light_pic" src={monitor_light1} />
+        <div className="monitor_top2">40%</div>
       </div>
 
       <div className="wrap_detail">
-        <img className="monitor_detail_pic" src={monitor_detail}/>
-          <div className="monitor_top6">隨便亂打:))*^$#%$%/\';$%</div>
+        <img className="monitor_detail_pic" src={monitor_detail} />
+        <div className="monitor_top6">隨便亂打:))*^$#%$%/\';$%</div>
       </div>
 
-        <img className="monitor_arrow_left" src={monitor_arrow_left}/>
+      <img className="monitor_arrow_left" src={monitor_arrow_left} />
 
       <div className="wrap_temp">
-        <img className="monitor_temp_pic" src={monitor_temperature}/>
-          <div className="monitor_top4">37°c</div>
+        <img className="monitor_temp_pic" src={monitor_temperature} />
+        <div className="monitor_top4">37°c</div>
       </div>
 
       <div className="wrap_humi">
-        <img className="monitor_humi_pic" src={monitor_humidity}/>
-          <div className="monitor_top5">35%</div>
+        <img className="monitor_humi_pic" src={monitor_humidity} />
+        <div className="monitor_top5">35%</div>
       </div>
 
-        <img className="monitor_arrow_right" src={monitor_arrow_right}/>
+      <img className="monitor_arrow_right" src={monitor_arrow_right} />
 
-        <div class="footer_monitor "></div>
+      <div class="footer_monitor "></div>
 
-      </div>   
+    </div>
   )
 }
 
-export default monitor;
+export default Monitor;
