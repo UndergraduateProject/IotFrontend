da6cc9228951fcc1beeefd09d41fcc83aeaa004f
Nyc <s2012439@yes.my>
2021-06-21 16:03:11 +0800

    fix control index

--- src/pages/control/index.js
+++ src/pages/control/index.js
@@ -9,7 +9,7 @@ import control_close from './control_close.png';
 
 
 
-function control() {
+function Control() {
   return (
     <div className="body_control container-fluid">
       <div className="row"><img className="control_house_pic" src={control_greenHouse}/></div>
@@ -46,4 +46,4 @@ function control() {
   )
 }
 
-export default control;
+export default Control;
