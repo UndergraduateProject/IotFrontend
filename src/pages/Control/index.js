31cb824e064ad2018e2787ca0c1877aa70f73832
Nyc <s2012439@yes.my>
2021-06-28 06:10:20 +0800

    control layout done

--- src/pages/Control/index.js
+++ src/pages/Control/index.js
@@ -14,12 +14,46 @@ import Sidebar from '../../component/Sidebar'
 function Control() {
   return (
     <Container fluid className="control-container">
-      <Sidebar />
+      <Row><Sidebar /></Row>
       <Row>
-        {/* <div><img className="house" src={control_greenHouse}/></div> */}
-        <div className="change-btn">Change</div>
+        <Col className="img"><img className="house" src={control_greenHouse}/></Col>
+        <Col className="change-btn">Change</Col>
       </Row>
-
+      <Row >
+        <Col className="catalogue">Catalogue</Col>
+      </Row>
+      <Row className="control-square">
+        <Col>
+          <div></div>
+          <div className="data">30%</div>
+          <div>Battery</div>
+        </Col>
+        <Col>
+          <div></div>
+          <div className="data">750ml</div>
+          <div>Capacity</div>
+        </Col>
+        <Col>
+          <div></div>
+          <div className="data">40%</div>
+          <div>Light</div>
+        </Col>
+      </Row>
+      <Row className="control-rectangle">
+        <Col>
+          <div></div>
+          <div className="data">30Min</div>
+          <div>Duration</div>
+        </Col>
+        <Col>
+          <div></div>
+          <div className="data">Open</div>
+          <div>Close</div>
+        </Col>
+      </Row>
+      <Row>
+        <button className="control_button2">Power On</button>
+      </Row>
     </Container>
   )
 }
