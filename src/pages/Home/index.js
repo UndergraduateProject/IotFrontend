c8b2532f5272eb3173bf1a89e62a3652199a2088
AngelHuang1998 <a7n1g1e1l@gmail.com>
2021-06-28 18:22:27 +0800

    forget password and homepage
    忘記密碼有三個頁面，目前用好第一個

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,12 +4,15 @@ import {Container, Row, Col, Button} from 'react-bootstrap';
 import "./homepage.css";
 import logo from "./logo.png";
 import Userstatus from "./Userstatus";
+import Sidebar from '../../component/Sidebar'
+
 
 
 function homepage() {
   return (
     <div class="home">
     <Container >
+    <Row><Sidebar /></Row>
       <Userstatus /> 
       <Row>
         <Col  className="home_top1" md={{ span: 6, offset: 3 }} sm={{ span: 6, offset: 3 }} xs={{ span: 6, offset: 3 }}>
@@ -34,10 +37,6 @@ function homepage() {
       <Row >
         <Col className="home_top2" md={{ span: 4, offset: 4 }} sm={{ span: 4, offset: 4 }} xs={{ span: 4, offset: 4 }}>
         <Button size="lg" variant="light" className="home_btn">分析結果</Button>{' '}</Col>
-      </Row>
-      
-      <Row>
-        <Col className="home_top3"></Col>
       </Row>
 
     </Container>
