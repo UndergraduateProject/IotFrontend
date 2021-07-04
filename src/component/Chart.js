b901e807af13aa84cf0832dfcb1f6f82d16038d6
Nyc <s2012439@yes.my>
2021-07-04 23:04:10 +0800

    chart

--- src/component/Chart.js
+++ src/component/Chart.js
@@ -3,63 +3,43 @@ import './chart.css';
 import { Line } from 'react-chartjs-2';
 import api from '../utils/api';
 
-const path = "api/Humidtemp/";
-
-const linedata = {
-  labels: [],
-  datasets: [
-    {
-      label: 'Humidity',
-      data: [],
-      fill: false,
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgba(255, 99, 132, 0.2)',
-      yAxisID: 'y-axis-1',
-    },
-    {
-      label: 'Temperature',
-      data: [],
-      fill: false,
-      backgroundColor: 'rgb(54, 162, 235)',
-      borderColor: 'rgba(54, 162, 235, 0.2)',
-      yAxisID: 'y-axis-2',
-    },
-  ],
-};
+const path = "api/Humidtemp/?limit=100";
 
 
 function Chart() {
-  const [data, setData] = useState([]);
-  const [view,setView]  = useState(true);
-  useEffect(() => {
-    getdata();
-  }, []);
-
+  const [chart, setChart] = useState({});
+  var labels = [];
+  var dataValue = [];
   const getdata = () => {
     api.get(path).then(response => {
       console.log(response);
-      let humidity = [];
-      response['results'].forEach(ele => humidity.push(ele));
-      humidity.forEach(
-      ele => {
-        linedata.datasets[0].data.push(ele.humidity)
-        linedata.datasets[1].data.push(ele.temperature)
-        linedata.labels.push(ele.created)
-      }
-    )
-    setData(humidity);
+      response['results'].forEach(ele => {
+        labels.push(ele.timestamp);
+        dataValue.push(ele.humidity);
+      });
+    setChart({
+      labels: labels,
+      datasets: [
+        {
+          label: 'Humidity',
+          data: dataValue,
+          fill: false,
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgba(255, 99, 132, 0.2)',
+          yAxisID: 'y-axis-1',
+        },
+      ],
+    })
     })
   };
+
+  useEffect(() => {
+    getdata();
+  }, []);
   
-  if(view){
-    var msg = "show chart";
-  }
-  else{
-    var msg = "show raw data";
-  }
   return (
     <div>
-      <Line  data={linedata} />
+      <Line  data={chart} />
     </div>
   )
 }
