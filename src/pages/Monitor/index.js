2b582a429e8a0a367ce006449f77d742d0bb6eac
AngelHuang1998 <a7n1g1e1l@gmail.com>
2021-07-04 22:58:16 +0800

    monitor page
    monitor page調好了，然後下面那個可以滑的部分本來是說要用個左右的箭頭，但我覺得可以用成像是手機頁面左滑又滑的那個點點，你覺得哩

--- src/pages/Monitor/index.js
+++ src/pages/Monitor/index.js
@@ -1,53 +1,65 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./monitor.css";
+import {Container, Row, Col} from 'react-bootstrap';
 import monitor_weather from './monitor_weather.png';
-
-import monitor_light1 from './monitor_light1.png';
-import monitor_temperature from './monitor_temperature.png';
-import monitor_humidity from './monitor_humidity.png';
-import monitor_detail from './monitor_detail.png';
-import monitor_arrow_left from './monitor_arrow_left.png';
-import monitor_arrow_right from './monitor_arrow_right.png';
-
+import light from './light.png';
+import cloud from './cloud.png';
+import water from './water.png';
+import Sidebar from '../../component/Sidebar'
 
 
 function Monitor() {
   return (
-    <div className="body_monitor">
+    <div className="monitor_body">
+      <Container>
+        <Row><Sidebar /></Row>
+          <Row>
+            <Col className="monitor_top1" md={{ span: 3, offset: 9 }}  sm={{ span: 3, offset: 9 }} xs={{ span: 3, offset: 9 }}>
+              <img className="monitor_wea_pic" src= { monitor_weather }/>
+            </Col>
+          </Row>
 
-      <div className="monitor_top">Green House</div>
-      <div className="monitor_top1">Monitoring Status</div>
+          <Row>
+            <Col className="monitor_top2"  md={{ span: 6, offset: 6 }}  sm={{ span: 6, offset: 6 }} xs={{ span: 6, offset: 6 }}>
+              晴時多雲
+            </Col>
+          </Row>
 
-      <img className="monitor_wea_pic" src={monitor_weather} />
-      <div className="monitor_top3">晴時多雲</div>
+          <Row>
+            <Col className="monitor_top3" md={12} sm={12} xs={12}>
+              "Monitoring Status"
+            </Col>
+          </Row>
 
+          <Row>
+            <Col md={12} sm={12} xs={12}>
+                <div className="monitor_background"> 
+                  <div className="monitor_light"> <div className="monitor_light2">light</div>
+                    <div><img className="monitor_light_pic" src={light}/></div>
+                    <div className="monitor_light1">40%</div>
+                  </div>
+                  <div className=" monitor_detail">這邊放各種資訊abcdsfwrfsfsdldf(亂打)</div>
+                </div>
+            </Col> 
+          </Row>
 
-      <div className="wrap_light">
-        <img className="monitor_light_pic" src={monitor_light1} />
-        <div className="monitor_top2">40%</div>
-      </div>
+          <Row>
+            <Col md={6} sm={6} xs={6}>
+              <div className="monitor_temp"> <div className="monitor_temp2">Temperature</div>
+                <div><img className="monitor_cloud_pic" src={cloud} /></div>
+                <div className="monitor_temp1">25°C</div>
+              </div>
+            </Col>
+            <Col md={6} sm={6} xs={6}>
+              <div className="monitor_humi"> <div className="monitor_humi2">Humidity</div>
+                <div><img className="monitor_water_pic" src={water} /></div>
+                <div className="monitor_humi1">35%</div>
+              </div>
+            </Col>
+          </Row> 
 
-      <div className="wrap_detail">
-        <img className="monitor_detail_pic" src={monitor_detail} />
-        <div className="monitor_top6">隨便亂打:))*^$#%$%/\';$%</div>
-      </div>
-
-      <img className="monitor_arrow_left" src={monitor_arrow_left} />
-
-      <div className="wrap_temp">
-        <img className="monitor_temp_pic" src={monitor_temperature} />
-        <div className="monitor_top4">37°c</div>
-      </div>
-
-      <div className="wrap_humi">
-        <img className="monitor_humi_pic" src={monitor_humidity} />
-        <div className="monitor_top5">35%</div>
-      </div>
-
-      <img className="monitor_arrow_right" src={monitor_arrow_right} />
-
-      <div class="footer_monitor "></div>
-
+      </Container>
     </div>
   )
 }
