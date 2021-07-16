28dc2d9625dd7340ee5a4ed3c4596a7f9743d8fb
angel <a7n1g1e1l@gmail.com>
2021-07-16 16:26:43 +0800

    7/16

--- src/pages/Select/index.js
+++ src/pages/Select/index.js
@@ -8,7 +8,6 @@ import select_tomato from "../../images/select_tomato.png";
 import Sidebar from '../../component/Sidebar';
 
 
-
 function select() {
   return (
     <div className="body_select">
@@ -17,8 +16,9 @@ function select() {
               <Row>
                 <Col  md={{ span: 10, offset: 1 }}  sm={{ span: 10, offset: 1 }} xs={{ span: 10, offset: 1 }}> 
                   <div className="select_plant" >
+                    {/* <div className="left"> */}
                       <Row>
-                        <Col md={6}  sm={6} xs={6}><img className="select_potato_pic" src={select_potato} alt=""/></Col>
+                        <Col md={6}  sm={6} xs={6}><img className="select_potato_pic" src={select_potato}/></Col>
                       </Row>
 
                       <Row>
@@ -34,6 +34,8 @@ function select() {
                         <Col className="select_level_ans" md={2} sm={2} xs={2}>EASY</Col>
                         <Col className="select_cost_ans" md={3} sm={3} xs={3}>$40</Col>
                       </Row>
+                    {/* </div>  */}
+                    {/* <div className="right"> */}
                       <Row>
                         <Col className="select_week" md={{ span: 6, offset: 6 }}  sm={{ span: 6, offset: 6 }} xs={{ span: 6, offset: 6 }}>5 Weeks</Col>
                       </Row>
@@ -54,6 +56,7 @@ function select() {
                       </Row>
 
                     </div>
+                  {/* </div> */}
                 </Col>
               </Row>
 
@@ -62,9 +65,10 @@ function select() {
 
               <Row>
                 <Col  md={{ span: 10, offset: 1 }}  sm={{ span: 10, offset: 1 }} xs={{ span: 10, offset: 1 }}> 
-                  <div className="select_plant select_plant2" >
+                  <div className="select_plant " >
+                    {/* <div className="left"> */}
                       <Row>
-                        <Col md={6}  sm={6} xs={6}><img className="select_potato_pic" src={select_cabbage} alt=""/></Col>
+                        <Col md={6}  sm={6} xs={6}><img className="select_potato_pic" src={select_cabbage}/></Col>
                       </Row>
 
                       <Row>
@@ -80,6 +84,8 @@ function select() {
                         <Col className="select_level_ans" md={2} sm={2} xs={2}>EASY</Col>
                         <Col className="select_cost_ans" md={3} sm={3} xs={3}>$100</Col>
                       </Row>
+                    {/* </div>  */}
+                    {/* <div className="right"> */}
                       <Row>
                         <Col className="select_week" md={{ span: 6, offset: 6 }}  sm={{ span: 6, offset: 6 }} xs={{ span: 6, offset: 6 }}>10 Weeks</Col>
                       </Row>
@@ -100,6 +106,7 @@ function select() {
                       </Row>
 
                     </div>
+                  {/* </div> */}
                 </Col>
               </Row>
 
@@ -108,9 +115,10 @@ function select() {
 
               <Row>
                 <Col  md={{ span: 10, offset: 1 }}  sm={{ span: 10, offset: 1 }} xs={{ span: 10, offset: 1 }}> 
-                  <div className="select_plant select_plant3" >
+                  <div className="select_plant" >
+                    {/* <div className="left"> */}
                       <Row>
-                        <Col md={6}  sm={6} xs={6}><img className="select_potato_pic" src={select_tomato} alt=""/></Col>
+                        <Col md={6}  sm={6} xs={6}><img className="select_potato_pic" src={select_tomato}/></Col>
                       </Row>
 
                       <Row>
@@ -126,6 +134,8 @@ function select() {
                         <Col className="select_level_ans" md={2} sm={2} xs={2}>HIGH</Col>
                         <Col className="select_cost_ans" md={3} sm={3} xs={3}>$60</Col>
                       </Row>
+                    {/* </div>  */}
+                    {/* <div className="right"> */}
                       <Row>
                         <Col className="select_week" md={{ span: 6, offset: 6 }}  sm={{ span: 6, offset: 6 }} xs={{ span: 6, offset: 6 }}>12 Weeks</Col>
                       </Row>
@@ -150,16 +160,200 @@ function select() {
                 </Col>
               </Row>
 
-             
-              <Row >                 
-                <Col  className="select_next_top" md={6}  sm={6} xs={6}>
-                <a  className="select_next1"herf="#"> {'<'}{'<'}上一頁</a> 
-                </Col> 
 
-                <Col  className="select_next_top2" md={6}  sm={6} xs={6}>
-                <a  className="select_next1"herf="#">下一頁{'>'}{'>'}</a> 
-                </Col> 
+
+              <Row>
+                <Col  md={{ span: 10, offset: 1 }}  sm={{ span: 10, offset: 1 }} xs={{ span: 10, offset: 1 }}> 
+                  <div className="select_plant " >
+                    {/* <div className="left"> */}
+                      <Row>
+                        <Col md={6}  sm={6} xs={6}><img className="select_potato_pic" src={select_tomato}/></Col>
+                      </Row>
+
+                      <Row>
+                        <Col className="select_title">Tomato</Col>
+                      </Row>
+
+                      <Row>
+                        <Col className="select_level" md={2} sm={2} xs={2}>Level</Col>
+                        <Col className="select_cost" md={3} sm={3} xs={3}>Cost</Col>
+                      </Row>
+
+                      <Row>
+                        <Col className="select_level_ans" md={2} sm={2} xs={2}>HIGH</Col>
+                        <Col className="select_cost_ans" md={3} sm={3} xs={3}>$60</Col>
+                      </Row>
+                    {/* </div>  */}
+                    {/* <div className="right"> */}
+                      <Row>
+                        <Col className="select_week" md={{ span: 6, offset: 6 }}  sm={{ span: 6, offset: 6 }} xs={{ span: 6, offset: 6 }}>12 Weeks</Col>
+                      </Row>
+
+                      <Row>
+                        <Col className="select_evaluation" md={{ span: 6, offset: 6 }}  sm={{ span: 6, offset: 6 }} xs={{ span: 6, offset: 6 }}>self-evaluation</Col>
+                      </Row>
+
+                      <Row>
+                        <Col className="select_evaluation_ans" md={{ span: 6, offset: 6 }}  sm={{ span: 6, offset: 6 }} xs={{ span: 6, offset: 6 }}>
+                        A rose is a woody perennial flowering plant of the genus Rosa.</Col>
+                      </Row>
+
+                      <Row>
+                        <Col md={{ span: 6, offset: 6 }}  sm={{ span: 6, offset: 6 }} xs={{ span: 6, offset: 6 }}>
+                        <Button size="sm"  className="select_btn">Select</Button>{' '}
+                        </Col>
+                      </Row>
+
+                    </div>
+                  {/* </div> */}
+                </Col>
               </Row>
+
+
+
+              <Row>
+                <Col  md={{ span: 10, offset: 1 }}  sm={{ span: 10, offset: 1 }} xs={{ span: 10, offset: 1 }}> 
+                  <div className="select_plant " >
+                    {/* <div className="left"> */}
+                      <Row>
+                        <Col md={6}  sm={6} xs={6}><img className="select_potato_pic" src={select_tomato}/></Col>
+                      </Row>
+
+                      <Row>
+                        <Col className="select_title">Tomato</Col>
+                      </Row>
+
+                      <Row>
+                        <Col className="select_level" md={2} sm={2} xs={2}>Level</Col>
+                        <Col className="select_cost" md={3} sm={3} xs={3}>Cost</Col>
+                      </Row>
+
+                      <Row>
+                        <Col className="select_level_ans" md={2} sm={2} xs={2}>HIGH</Col>
+                        <Col className="select_cost_ans" md={3} sm={3} xs={3}>$60</Col>
+                      </Row>
+                    {/* </div>  */}
+                    {/* <div className="right"> */}
+                      <Row>
+                        <Col className="select_week" md={{ span: 6, offset: 6 }}  sm={{ span: 6, offset: 6 }} xs={{ span: 6, offset: 6 }}>12 Weeks</Col>
+                      </Row>
+
+                      <Row>
+                        <Col className="select_evaluation" md={{ span: 6, offset: 6 }}  sm={{ span: 6, offset: 6 }} xs={{ span: 6, offset: 6 }}>self-evaluation</Col>
+                      </Row>
+
+                      <Row>
+                        <Col className="select_evaluation_ans" md={{ span: 6, offset: 6 }}  sm={{ span: 6, offset: 6 }} xs={{ span: 6, offset: 6 }}>
+                        A rose is a woody perennial flowering plant of the genus Rosa.</Col>
+                      </Row>
+
+                      <Row>
+                        <Col md={{ span: 6, offset: 6 }}  sm={{ span: 6, offset: 6 }} xs={{ span: 6, offset: 6 }}>
+                        <Button size="sm"  className="select_btn">Select</Button>{' '}
+                        </Col>
+                      </Row>
+
+                    </div>
+                  {/* </div> */}
+                </Col>
+              </Row>
+
+
+              <Row>
+                <Col  md={{ span: 10, offset: 1 }}  sm={{ span: 10, offset: 1 }} xs={{ span: 10, offset: 1 }}> 
+                  <div className="select_plant " >
+                    {/* <div className="left"> */}
+                      <Row>
+                        <Col md={6}  sm={6} xs={6}><img className="select_potato_pic" src={select_tomato}/></Col>
+                      </Row>
+
+                      <Row>
+                        <Col className="select_title">Tomato</Col>
+                      </Row>
+
+                      <Row>
+                        <Col className="select_level" md={2} sm={2} xs={2}>Level</Col>
+                        <Col className="select_cost" md={3} sm={3} xs={3}>Cost</Col>
+                      </Row>
+
+                      <Row>
+                        <Col className="select_level_ans" md={2} sm={2} xs={2}>HIGH</Col>
+                        <Col className="select_cost_ans" md={3} sm={3} xs={3}>$60</Col>
+                      </Row>
+                    {/* </div>  */}
+                    {/* <div className="right"> */}
+                      <Row>
+                        <Col className="select_week" md={{ span: 6, offset: 6 }}  sm={{ span: 6, offset: 6 }} xs={{ span: 6, offset: 6 }}>12 Weeks</Col>
+                      </Row>
+
+                      <Row>
+                        <Col className="select_evaluation" md={{ span: 6, offset: 6 }}  sm={{ span: 6, offset: 6 }} xs={{ span: 6, offset: 6 }}>self-evaluation</Col>
+                      </Row>
+
+                      <Row>
+                        <Col className="select_evaluation_ans" md={{ span: 6, offset: 6 }}  sm={{ span: 6, offset: 6 }} xs={{ span: 6, offset: 6 }}>
+                        A rose is a woody perennial flowering plant of the genus Rosa.</Col>
+                      </Row>
+
+                      <Row>
+                        <Col md={{ span: 6, offset: 6 }}  sm={{ span: 6, offset: 6 }} xs={{ span: 6, offset: 6 }}>
+                        <Button size="sm"  className="select_btn">Select</Button>{' '}
+                        </Col>
+                      </Row>
+
+                    </div>
+                  {/* </div> */}
+                </Col>
+              </Row>
+
+
+              <Row>
+                <Col  md={{ span: 10, offset: 1 }}  sm={{ span: 10, offset: 1 }} xs={{ span: 10, offset: 1 }}> 
+                  <div className="select_plant" >
+                    {/* <div className="left"> */}
+                      <Row>
+                        <Col md={6}  sm={6} xs={6}><img className="select_potato_pic" src={select_potato}/></Col>
+                      </Row>
+
+                      <Row>
+                        <Col className="select_title">Potato</Col>
+                      </Row>
+
+                      <Row>
+                        <Col className="select_level" md={2} sm={2} xs={2}>Level</Col>
+                        <Col className="select_cost" md={3} sm={3} xs={3}>Cost</Col>
+                      </Row>
+
+                      <Row>
+                        <Col className="select_level_ans" md={2} sm={2} xs={2}>EASY</Col>
+                        <Col className="select_cost_ans" md={3} sm={3} xs={3}>$40</Col>
+                      </Row>
+                    {/* </div>  */}
+                    {/* <div className="right"> */}
+                      <Row>
+                        <Col className="select_week" md={{ span: 6, offset: 6 }}  sm={{ span: 6, offset: 6 }} xs={{ span: 6, offset: 6 }}>5 Weeks</Col>
+                      </Row>
+
+                      <Row>
+                        <Col className="select_evaluation" md={{ span: 6, offset: 6 }}  sm={{ span: 6, offset: 6 }} xs={{ span: 6, offset: 6 }}>self-evaluation</Col>
+                      </Row>
+
+                      <Row>
+                        <Col className="select_evaluation_ans" md={{ span: 6, offset: 6 }}  sm={{ span: 6, offset: 6 }} xs={{ span: 6, offset: 6 }}>
+                          I am Potato,The potato is a root vegetable native to the America.</Col>
+                      </Row>
+
+                      <Row>
+                        <Col md={{ span: 6, offset: 6 }}  sm={{ span: 6, offset: 6 }} xs={{ span: 6, offset: 6 }}>
+                        <Button size="sm"  className="select_btn">Select</Button>{' '}
+                        </Col>
+                      </Row>
+
+                    </div>
+                  {/* </div> */}
+                </Col>
+              </Row>
+
               
       </Container>
     </div> 
