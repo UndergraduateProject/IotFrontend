1804231c9eaeb8f5b07bb40bb9757bd6a6152c43
angel <a7n1g1e1l@gmail.com>
2021-08-07 19:59:29 +0800

    profile page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Visualization from "./pages/Visualization";
 import Warning_center from "./pages/Warning_center";
 import Setting from "./pages/Setting";
 import Detect from "./pages/Detect";
+import Profile from "./pages/Profile";
+
 import {
   Redirect,
   Route,
@@ -43,6 +45,7 @@ function App() {
         <ProtectedRoute path="/warning_center" loggedIn = {log} component = {Warning_center}/>
         <ProtectedRoute path="/setting" loggedIn = {log} component = {Setting}/>
         <ProtectedRoute path="/detect" loggedIn = {log} component = {Detect}/>
+        <ProtectedRoute path="/profile" loggedIn = {log} component = {Profile}/>
         <Route path="/">{log ? <Homepage /> :<Login  state={{log :[log,setLog]}}/>}</Route>
       </Switch>
       
