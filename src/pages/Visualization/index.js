85c754c643a4fca2a8b0fd81ae36a46519533a2c
Nyc <s2012439@yes.my>
2021-08-15 01:51:52 +0800

    delete underline, visualization css

--- src/pages/Visualization/index.js
+++ src/pages/Visualization/index.js
@@ -8,7 +8,7 @@ import moment from 'moment';
 import Sidebar from "../../component/Sidebar";
 
 
-const path = "api/Humidtemp/?limit=10000";
+const path = "api/Humidtemp/?limit=99999999";
 
 
 
@@ -30,7 +30,7 @@ function Visualization() {
 			}
 			
           },
-          colors: ['#008FFB'],
+          colors: ['#4B83D1'],
           yaxis: {
             labels: {
               minWidth: 40
@@ -43,15 +43,15 @@ function Visualization() {
         noData:{
             text: "DataLarge...Loading..."
         },
-		title:{
-			text:"Humidity",
-			style: {
-				fontSize:  '28px',
-				fontWeight:  'bold',
-				fontFamily:  "SF Pro Display",
-				color:  '#FFFFFF'
-			  },
-		}
+		// title:{
+		// 	text:"Humidity",
+		// 	style: {
+		// 		fontSize:  '28px',
+		// 		fontWeight:  'bold',
+		// 		fontFamily:  "SF Pro Display",
+		// 		color:  '#FFFFFF'
+		// 	  },
+		// }
       },
 		tempoptions: {
         chart: {
@@ -68,7 +68,7 @@ function Visualization() {
 				autoSelected: 'zoom'
 			}
           },
-          colors: ['#546E7A'],
+          colors: ['#FF3F7E'],
           yaxis: {
             labels: {
               minWidth: 40,
@@ -81,155 +81,173 @@ function Visualization() {
         noData:{
             text: "DataLarge...Loading..."
         },
-		title:{
-			text:"Temperature",
-			style: {
-				fontSize:  '28px',
-				fontWeight:  'bold',
-				fontFamily:  "SF Pro Display",
-				color:  '#FFFFFF'
-			  },
-		}
+		// title:{
+		// 	text:"Temperature",
+		// 	style: {
+		// 		fontSize:  '28px',
+		// 		fontWeight:  'bold',
+		// 		fontFamily:  "SF Pro Display",
+		// 		color:  '#FFFFFF'
+		// 	  },
+		// }
       },
 	
       humidityseries: [
         {
           name: "humidity",
-          data: [],
+          data: [{ x: '01/06/2021', y: 54 },{ x: '02/08/2021', y: 17 },  {x: '03/28/2021', y: 26 },{ x: '04/06/2021', y: 54 },{ x: '05/08/2021', y: 17 },  {x: '06/28/2021', y: 26 },{ x: '07/06/2021', y: 54 },{ x: '08/08/2021', y: 17 },  {x: '09/28/2021', y: 26 },{ x: '10/06/2021', y: 54 },{ x: '11/08/2021', y: 17 },  {x: '12/28/2021', y: 26 }]
         }
       ],
-			tempseries:[
-				{
-					name: "temperature",
-					data: [],
-				}
-			]
+		tempseries:[
+			{
+				name: "temperature",
+				data: [{ x: '01/06/2021', y: 54 },{ x: '02/08/2021', y: 17 },  {x: '03/28/2021', y: 26 },{ x: '04/06/2021', y: 54 },{ x: '05/08/2021', y: 17 },  {x: '06/28/2021', y: 26 },{ x: '07/06/2021', y: 54 },{ x: '08/08/2021', y: 17 },  {x: '09/28/2021', y: 26 },{ x: '10/06/2021', y: 54 },{ x: '11/08/2021', y: 17 },  {x: '12/28/2021', y: 26 }]
+			}
+		],
+		current : 0,
+		selection : 'one_month',
+		key: new Date(),
     }
   )
 	
 	const [temp, setTemp] = useState({});
 	const [humidity, setHumidity] = useState({});
+	const tempChart = <Chart
+		options={state.tempoptions}
+		series={state.tempseries}
+		type="line"
+		width="100%"
+	/>;
 
-  useEffect(() => {
-	var tempValue = {
-		"weekly" : [],
-		"daily" : [],
-		"hourly" : [],
-	};
+  	const humidChart = <Chart
+		options={state.humidoptions}
+		series={state.humidityseries}
+		type="line"
+		width="100%"
+		/>;
+	const [chart, setChart] = useState(0)
+	
 
-	var humidityValue = {
-		"weekly" : [],
-		"daily" : [],
-		"hourly" : [],
-	};
-		var tmp = {};
-		var hourTmp = {};
-		var weekTmp = {};
-		const getdata = async () => {
-			api.get(path).then(response => {
-				response['results'].forEach(ele => {
-					const time = new Date(ele.timestamp);
-					const date = time.toISOString().split('T')[0];
-					const obj = tmp[date] = tmp[date] || {count:0, temperature:0, humidity:0};
-					const yearWeek = [moment(date).year(),moment(date).week()];
-					const yearDay = [moment(date).year(),
-						(moment(date).month()+1).toLocaleString('en-US',{minimumIntegerDigits:2, useGrouping:false}),
-						moment(date).date().toLocaleString('en-US',{minimumIntegerDigits:2, useGrouping:false}),
-						moment(time).hour().toLocaleString('en-US',{minimumIntegerDigits:2, useGrouping:false})];
-					const hourObj = hourTmp[yearDay] = hourTmp[yearDay] || {count:0, temperature:0,humidity:0};
-					const weekObj = weekTmp[yearWeek] = weekTmp[yearWeek] || {count:0, temperature:0, humidity:0};
-					obj.count++;
-					obj.temperature += ele.temperature;
-					obj.humidity += ele.humidity;
-					hourObj.count++;
-					hourObj.temperature += ele.temperature;
-					hourObj.humidity += ele.humidity;
-					weekObj.count++;
-					weekObj.temperature += ele.temperature;
-					weekObj.humidity += ele.humidity;
-				});
+//   useEffect(() => {
+// 	var tempValue = {
+// 		"weekly" : [],
+// 		"daily" : [],
+// 		"hourly" : [],
+// 	};
+
+// 	var humidityValue = {
+// 		"weekly" : [],
+// 		"daily" : [],
+// 		"hourly" : [],
+// 	};
+// 		var tmp = {};
+// 		var hourTmp = {};
+// 		var weekTmp = {};
+// 		const getdata = async () => {
+// 			api.get(path).then(response => {
+// 				response['results'].forEach(ele => {
+// 					const time = new Date(ele.timestamp);
+// 					const date = time.toISOString().split('T')[0];
+// 					const obj = tmp[date] = tmp[date] || {count:0, temperature:0, humidity:0};
+// 					const yearWeek = [moment(date).year(),moment(date).week()];
+// 					const yearDay = [moment(date).year(),
+// 						(moment(date).month()+1).toLocaleString('en-US',{minimumIntegerDigits:2, useGrouping:false}),
+// 						moment(date).date().toLocaleString('en-US',{minimumIntegerDigits:2, useGrouping:false}),
+// 						moment(time).hour().toLocaleString('en-US',{minimumIntegerDigits:2, useGrouping:false})];
+// 					const hourObj = hourTmp[yearDay] = hourTmp[yearDay] || {count:0, temperature:0,humidity:0};
+// 					const weekObj = weekTmp[yearWeek] = weekTmp[yearWeek] || {count:0, temperature:0, humidity:0};
+// 					obj.count++;
+// 					obj.temperature += ele.temperature;
+// 					obj.humidity += ele.humidity;
+// 					hourObj.count++;
+// 					hourObj.temperature += ele.temperature;
+// 					hourObj.humidity += ele.humidity;
+// 					weekObj.count++;
+// 					weekObj.temperature += ele.temperature;
+// 					weekObj.humidity += ele.humidity;
+// 				});
 				
-				const res = Object.entries(tmp).map(function(entry){
-					return { date: entry[0], tempAvg: (entry[1].temperature/entry[1].count).toFixed(2), humidAvg: (entry[1].humidity/entry[1].count).toFixed(2)}
-				});
+// 				const res = Object.entries(tmp).map(function(entry){
+// 					return { date: entry[0], tempAvg: (entry[1].temperature/entry[1].count).toFixed(2), humidAvg: (entry[1].humidity/entry[1].count).toFixed(2)}
+// 				});
 
-				const hourlyRes = Object.entries(hourTmp).map(function(entry){
-					entry[0] = entry[0].replace(',','-');
-					entry[0] = entry[0].replace(',','-');
-					entry[0] = entry[0].replace(',','T');
-					entry[0] = moment(entry[0]);
-					// entry[0] = new Date(entry[0]).getTime();
-					return { date: entry[0], tempAvg: (entry[1].temperature/entry[1].count).toFixed(2), humidAvg: (entry[1].humidity/entry[1].count).toFixed(2)}
-				});
+// 				const hourlyRes = Object.entries(hourTmp).map(function(entry){
+// 					entry[0] = entry[0].replace(',','-');
+// 					entry[0] = entry[0].replace(',','-');
+// 					entry[0] = entry[0].replace(',','T');
+// 					entry[0] = moment(entry[0]);
+// 					// entry[0] = new Date(entry[0]).getTime();
+// 					return { date: entry[0], tempAvg: (entry[1].temperature/entry[1].count).toFixed(2), humidAvg: (entry[1].humidity/entry[1].count).toFixed(2)}
+// 				});
 
-				const weekRes = Object.entries(weekTmp).map(function(entry){
-					entry[0] = entry[0].replace(',','W');
-					entry[0] = moment(entry[0]);
-					return { date: entry[0], tempAvg: (entry[1].temperature/entry[1].count).toFixed(2), humidAvg: (entry[1].humidity/entry[1].count).toFixed(2)}
-				});
+// 				const weekRes = Object.entries(weekTmp).map(function(entry){
+// 					entry[0] = entry[0].replace(',','W');
+// 					entry[0] = moment(entry[0]);
+// 					return { date: entry[0], tempAvg: (entry[1].temperature/entry[1].count).toFixed(2), humidAvg: (entry[1].humidity/entry[1].count).toFixed(2)}
+// 				});
 
 
-				weekRes.forEach(ele=>{
-					var temp = [];
-					var humidity = [];
-					humidity.push(ele.date);
-					humidity.push(ele.humidAvg);
-					temp.push(ele.date);
-					temp.push(ele.tempAvg);
-					tempValue.weekly.push(temp);
-					humidityValue.weekly.push(humidity)
-				})
+// 				weekRes.forEach(ele=>{
+// 					var temp = [];
+// 					var humidity = [];
+// 					humidity.push(ele.date);
+// 					humidity.push(ele.humidAvg);
+// 					temp.push(ele.date);
+// 					temp.push(ele.tempAvg);
+// 					tempValue.weekly.push(temp);
+// 					humidityValue.weekly.push(humidity)
+// 				})
 				
-				res.forEach(ele=>{
-					var temp = [];
-					var humidity = [];
-					humidity.push(ele.date);
-					humidity.push(ele.humidAvg);
-					temp.push(ele.date);
-					temp.push(ele.tempAvg);
-					tempValue.daily.push(temp);
-					humidityValue.daily.push(humidity)
-				})
+// 				res.forEach(ele=>{
+// 					var temp = [];
+// 					var humidity = [];
+// 					humidity.push(ele.date);
+// 					humidity.push(ele.humidAvg);
+// 					temp.push(ele.date);
+// 					temp.push(ele.tempAvg);
+// 					tempValue.daily.push(temp);
+// 					humidityValue.daily.push(humidity)
+// 				})
 
 				
-				hourlyRes.forEach(ele=>{
-					var temp = [];
-					var humidity = [];
-					humidity.push(ele.date);
-					humidity.push(ele.humidAvg);
-					temp.push(ele.date);
-					temp.push(ele.tempAvg);
-					tempValue.hourly.push(temp);
-					humidityValue.hourly.push(humidity)
-				})
+// 				hourlyRes.forEach(ele=>{
+// 					var temp = [];
+// 					var humidity = [];
+// 					humidity.push(ele.date);
+// 					humidity.push(ele.humidAvg);
+// 					temp.push(ele.date);
+// 					temp.push(ele.tempAvg);
+// 					tempValue.hourly.push(temp);
+// 					humidityValue.hourly.push(humidity)
+// 				})
 				
-				setTemp(tempValue);
-				setHumidity(humidityValue);
-				setState({
-						tempoptions: {
-								...state.tempoptions,
-							},
-						tempseries: [
-								{
-									...state.tempseries,
-									data: tempValue.daily,
-								}
-							],
+// 				setTemp(tempValue);
+// 				setHumidity(humidityValue);
+// 				setState({
+// 						tempoptions: {
+// 								...state.tempoptions,
+// 							},
+// 						tempseries: [
+// 								{
+// 									...state.tempseries,
+// 									data: tempValue.daily,
+// 								}
+// 							],
 						
-						humidoptions:{
-							...state.humidoptions,
-						},
-						humidityseries:[
-							{
-								...state.humidityseries,
-								data: humidityValue.daily,
-							}
-						]
-						})
-			})
-		};
-    getdata();
-  }, []);
+// 						humidoptions:{
+// 							...state.humidoptions,
+// 						},
+// 						humidityseries:[
+// 							{
+// 								...state.humidityseries,
+// 								data: humidityValue.daily,
+// 							}
+// 						]
+// 						})
+// 			})
+// 		};
+//     getdata();
+//   }, []);
 
 
   const changePeriod = (period) => {
@@ -320,95 +338,198 @@ function Visualization() {
 	}
   }
 
-  function changeDate(timeline){
-	setState({
-		selection: timeline
-	})
-	const today = new Date()
+	const changechart = (e) => {
+		switch (e.target.value){
+			case "0":
+				setChart(0)
+				setState({
+					...state,
+					current :0
+				})
+				break;
+			
+			case "1":
+				setChart(1)
+				setState({
+					...state,
+					current :1
+				})
+				break;
+			
+			case "2":
+				setChart(0)
+				setState({
+					...state,
+					current :0
+				})
+				break;
+			
+			default :
+				setChart(0)
+				setState({
+					...state,
+					current :0
+				})
+				break;
+		}
+	}
 
+  function changeDate(timeline){
+	const today = new Date();
 	switch (timeline) {
 		case 'one_month':
 			setState({
+				...state,
 				humidoptions:{
+					...state.humidoptions,
 					xaxis:{
 						min: new Date(today.getFullYear(), today.getMonth()-1, today.getDate()).getTime(),
 						max: today,
 					}
-				}
+				},
+				tempoptions:{
+					...state.tempoptions,
+					xaxis:{
+						min: new Date(today.getFullYear(), today.getMonth()-1, today.getDate()).getTime(),
+						max: today,
+					}
+				},
+			selection: timeline,
 			})
 
 			break;
 		case 'six_months':
 			setState({
+				...state,
 				humidoptions:{
+					...state.humidoptions,
 					xaxis:{
 						min: new Date(today.getFullYear(), today.getMonth()-6, today.getDate()).getTime(),
 						max: today,
 					}
-				}
+				},
+				tempoptions:{
+					...state.tempoptions,
+					xaxis:{
+						min: new Date(today.getFullYear(), today.getMonth()-6, today.getDate()).getTime(),
+						max: today,
+					}
+				},
+				selection: timeline,
 			})
 			break;
 		case 'one_year':
 			setState({
+				...state,
 				humidoptions:{
+					...state.humidoptions,
 					xaxis:{
 						min: new Date(today.getFullYear()-1, today.getMonth(), today.getDate()).getTime(),
 						max: today,
 					}
-				}
+				},
+				tempoptions:{
+					...state.tempoptions,
+					xaxis:{
+						min: new Date(today.getFullYear()-1, today.getMonth(), today.getDate()).getTime(),
+						max: today,
+					}
+				},
+				selection: timeline,
 			})
 			break;
 		case 'ytd':
 			setState({
+				...state,
 				humidoptions:{
+					...state.humidoptions,
 					xaxis:{
 						min: new Date(today.getFullYear(), today.getMonth(), today.getDate()-1).getTime(),
 						max: today,
 					}
-				}
+				},
+				tempoptions:{
+					...state.tempoptions,
+					xaxis:{
+						min: new Date(today.getFullYear(), today.getMonth(), today.getDate()-1).getTime(),
+						max: today,
+					}
+				},
+				selection: timeline,
 			})
 			break;
 		case 'all':
 			setState({
+				...state,
 				humidoptions:{
+					...state.humidoptions,
 					xaxis:{
 						min: null,
 						max: null,
 					}
-				}
+				},
+				tempoptions:{
+					...state.tempoptions,
+					xaxis:{
+						min: null,
+						max: null,
+					}
+				},
+				selection: timeline,
 			})
 			break;
 		default:
+			setState({
+				...state,
+				humidoptions:{
+					...state.humidoptions,
+					xaxis:{
+						min: null,
+						max: null,
+					}
+				},
+				tempoptions:{
+					...state.tempoptions,
+					xaxis:{
+						min: null,
+						max: null,
+					}
+				},
+				selection: timeline,
+			})
 	}
 }
-  
 
   return (
     <div className="visual-wrapper">
 		<Sidebar />
-		<div id="chart-temperature">
-			<Chart
-				options={state.tempoptions}
-				series={state.tempseries}
-				type="line"
-				width="100%"
-			/>
+		<div className="select-custom">
+		<select onChange={changechart}>
+			<option value="">Select: </option>
+			<option value="0">Temperature</option>
+			<option value="1">Humidity</option>
+			<option value="2">Moisture</option>
+		</select>
+		</div>
+		<div className="toolbar">
+        	<div><button onClick={()=>changeDate('one_month')} id="one_month" className={ (state.selection=='one_month' ? 'active' : '')}>1M</button></div>
+        	<div><button onClick={()=>changeDate('six_months')} id="six_months" className={ (state.selection=='six_months' ? 'active' : '')}>6M</button></div>
+        	<div><button onClick={()=>changeDate('one_year')} id="one_year" className={ (state.selection=='one_year' ? 'active' : '')}>1Y</button></div>
+        	<div><button onClick={()=>changeDate('ytd')} id="ytd" className={ (state.selection=='ytd' ? 'active' : '')}>YTD</button></div>
+        	<div><button onClick={()=>changeDate('all')} id="all" className={ (state.selection=='all' ? 'active' : '')}>ALL</button></div>
         </div>
-		<div id="chart-humidity">
-			<Chart
-				options={state.humidoptions}
-				series={state.humidityseries}
-				type="line"
-				width="100%"
-			/>
-        </div>
-	    <div className="toolbar">
-        	<div><button onClick={()=>changeDate('one_month')} id="one_month" className={ (state.selection==='one_month' ? 'active' : '')}>1M</button></div>
-        	<div><button onClick={()=>changeDate('six_months')} id="six_months" className={ (state.selection==='six_months' ? 'active' : '')}>6M</button></div>
-        	<div><button onClick={()=>changeDate('one_year')} id="one_year" className={ (state.selection==='one_year' ? 'active' : '')}>1Y</button></div>
-        	<div><button onClick={()=>changeDate('ytd')} id="ytd" className={ (state.selection==='ytd' ? 'active' : '')}>YTD</button></div>
-        	<div><button onClick={()=>changeDate('all')} id="all" className={ (state.selection==='all' ? 'active' : '')}>ALL</button></div>
-        </div>
+		<div id="chart">
+			{(()=>{
+				switch(chart){
+					case 0:
+						return(tempChart);
+					
+					case 1:
+						return(humidChart);
+				}
+			})()}
+    </div>
+		
 		<div className="toolbar">
 			<div><button onClick={()=>changePeriod("weekly")}>Weekly</button></div>
 			<div><button onClick={()=>changePeriod("daily")}>Daily</button></div>
