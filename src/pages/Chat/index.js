3304fb10e38262561b5b59af489caa1785ef4dd8
Nyc <s2012439@yes.my>
2021-09-11 18:12:37 +0800

    chat ui package.json conflict merge

--- src/pages/Chat/index.js
+++ src/pages/Chat/index.js
@@ -6,7 +6,8 @@ import "./chat.css"
 import user_icon from "../../images/user.png";
 
 export default function Chatbox() {
-  const [message, setMessage] = useState([new Message({id:1 ,message: 'Hello World!'}), new Message({id:0, message:"Hi!"})])
+  const [message, setMessage] = useState([new Message({id:1 ,message: 'Hello World!'})])
+  const [current, setCurrent] = useState(1);
   var input = useRef();
 
   const onMessageSubmit = (e) => {
@@ -16,6 +17,7 @@ export default function Chatbox() {
       return false;
     }
     pushMessage(0, data.value);
+    setCurrent(1)
     data.value = '';
     return true;
   }
@@ -29,37 +31,39 @@ export default function Chatbox() {
     setMessage([...message, newMessage]);
   }
 
+  const template = (<Row className="chat">
+  {/* <img className="chat-icon" src={user_icon} alt="profile_pic" /> */}
+  <ChatFeed
+  messages={message} // Array: list of message objects
+  isTyping={false} // Boolean: is the recipient typing
+  hasInputField={false} // Boolean: use our input, or use your own
+  showSenderName // show the name of the user who sent the message
+  bubblesCentered={false} //Boolean should the bubbles be centered in the feed?
+  // JSON: Custom bubble styles
+  bubbleStyles={
+    {
+      text: {
+        fontSize: "1rem"
+      },
+      chatbubble: {
+        borderRadius: 70,
+        padding: 10
+      }
+    }
+  }
+  />
+</Row>)
+
   return(
     <div className="chatbox">
       <Sidebar />
       <Row id="title">
         <Col >IRIS</Col>
       </Row>
-      <Row className="chat">
-        <img className="chat-icon" src={user_icon} alt="profile_pic" />
-        <ChatFeed
-        messages={message} // Array: list of message objects
-        isTyping={false} // Boolean: is the recipient typing
-        hasInputField={false} // Boolean: use our input, or use your own
-        showSenderName // show the name of the user who sent the message
-        bubblesCentered={false} //Boolean should the bubbles be centered in the feed?
-        // JSON: Custom bubble styles
-        bubbleStyles={
-          {
-            text: {
-              fontSize: "1rem"
-            },
-            chatbubble: {
-              borderRadius: 70,
-              padding: 10
-            }
-          }
-        }
-        />
-      </Row>
-      <form >
+      {template}
+      <form id="message-form">
         <input ref={m => input = m} id="input" placeholder="Type your message..."></input>
-        <button onClick={onMessageSubmit}>test</button>
+        <button id="send-btn" onClick={onMessageSubmit}>send</button>
       </form>
     </div>
   )
